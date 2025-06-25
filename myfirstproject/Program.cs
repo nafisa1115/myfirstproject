651d7b17679bbc4a62a16dfd13f8d9b3e748de70
nafisa1115 <nafisamaxamed819@gmail.com>
2025-06-25 18:02:27 +0300

    magac

--- myfirstproject/Program.cs
+++ myfirstproject/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Mala fahmay casharka !");
+
+string name = "ali farax gedi";
+Console.WriteLine(name);
+
+   
