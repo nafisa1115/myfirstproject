fab945355802ba386cd41ce4983a726f49a84410
nafisa1115 <nafisamaxamed819@gmail.com>
2025-06-26 17:13:32 +0300

    ASSIGMENT

--- myfirstproject/Program.cs
+++ myfirstproject/Program.cs
@@ -1,8 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Mala fahmay casharka !");
 
-string name = "ali farax gedi";
+
+string name = "nafisa mohamed ali";
 Console.WriteLine(name);
+string Mname = "rahma mohamed yusuf";
+Console.WriteLine(Mname);
+string DOP = "3/3/200";
+Console.WriteLine(DOP);
+string POB = "Galkacyo";
+Console.WriteLine(POB);
+string address = "garsoor";
+Console.WriteLine(address);
+int telephone = 675354678;
+Console.WriteLine(telephone);
+string sex = "female";
+Console.WriteLine(sex);
+string status = "single";
+Console.WriteLine(status);
 
-   jhkujhuo
